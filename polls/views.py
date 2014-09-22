05d88a601c859c694a87e018906fd05fca6ed88a
sEver <sever@users.noreply.github.com>
2014-09-22 11:23:45 +0200

    Rewritten some views to user return render

--- polls/views.py
+++ polls/views.py
@@ -3,7 +3,11 @@ from polls.models import Question
 from django.template import RequestContext, loader
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404
+    return render(request, 'polls/detail.html',{'question':question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
@@ -14,8 +18,5 @@ def vote(request, question_id):
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = RequestContext(request, {
-            'latest_question_list': latest_question_list,
-    })
-    return HttpResponse(template.render(context))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
